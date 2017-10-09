b2b2988e0e8fe7a67c6210eb430c59a8c7e3f36a
Alex Ruiz <swiminportals@gmail.com>
2017-10-08 19:41:48 -0700

    Added background sprite
    Lowell football field

--- game.js
+++ game.js
@@ -1,5 +1,27 @@
+//Initialize PIXI by creating a renderer and stage
 var renderer = PIXI.autoDetectRenderer(640, 480);
-
-document.body.appendChild(renderer.view);
 var stage = new PIXI.Container();
-renderer.render(stage);
+
+//Adds renderer to the website
+document.body.appendChild(renderer.view);
+
+
+//Load all the images needed for the game
+PIXI.loader
+  .add("media/ldr_bg1.png")
+  .load(setup);
+
+//This function runs when all the images have been loaded
+function setup() {
+  //Create background sprite
+  var bg = new PIXI.Sprite(
+    PIXI.loader.resources["media/ldr_bg1.png"].texture
+  );
+
+  //Add the sprite to the stage
+  stage.addChild(bg);
+  bg.visible = true;
+
+  //Render the game!
+  renderer.render(stage);
+}
